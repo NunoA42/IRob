fe391f382bc7db063b3585219641eb61729cf82a
p4bl1to072 <pablocrassous@gmail.com>
2025-09-17 15:52:59 +0200

    Added ros workspace

---
+++ IRob_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pablo/IRob_ws/src
+# Build directory: /home/pablo/IRob_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_datasets")
---
+++ IRob_ws/build/turtlebot3_datasets/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pablo/IRob_ws/src/turtlebot3_datasets
+# Build directory: /home/pablo/IRob_ws/build/turtlebot3_datasets
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
