5a31fa932a0cab1fc90096d7228dcb358b24e41b
Nuno Aguilar <nunoaguilar6@gmail.com>
2025-10-23 14:40:26 +0000

    update q learning

--- catkin_ws/src/q_learning_robot/scripts/q_learning_node.py
+++ catkin_ws/src/q_learning_robot/scripts/q_learning_node.py
@@ -133,7 +133,6 @@ class QLearningROS:
                     self.grid[r, c] = 1
 
         # Definição dos dois retângulos
-
         # Retângulo 1
         x1_min, x1_max = -1.5, 0.7
         y1_min, y1_max = 1.85, 4.3
@@ -142,24 +141,29 @@ class QLearningROS:
         x2_min, x2_max = 0.14, 3.9
         y2_min, y2_max = -1.3, -0.14
 
-        x3_min, x3_max = -0.65, 3.84
-        y3_min, y3_max = 0.855, 1.66
+        # Retângulo 3
+        x3_min, x3_max = -0.61, 4.19
+        y3_min, y3_max = 0.54, 1.66
+
+        # Retângulo 4 (novo)
+        x4_min, x4_max = -1.3170, 4.4620
+        y4_min, y4_max = 0.5298, 2.5689
 
         # Dimensões reais de cada célula da grelha
         cell_width_m = (self.map_info.width / self.GRID_SIZE) * self.map_info.resolution
         cell_height_m = (self.map_info.height / self.GRID_SIZE) * self.map_info.resolution
 
-        # Marcar obstáculos dentro destes dois retângulos
+        # Marcar obstáculos dentro destes retângulos
         for r in range(self.GRID_SIZE):
             for c in range(self.GRID_SIZE):
                 x_celula = c * cell_width_m + self.map_info.origin.position.x
                 y_celula = r * cell_height_m + self.map_info.origin.position.y
 
-                # Verifica se está dentro de algum dos dois retângulos
                 if (
                     (x1_min <= x_celula <= x1_max and y1_min <= y_celula <= y1_max) or
                     (x2_min <= x_celula <= x2_max and y2_min <= y_celula <= y2_max) or
-                    (x3_min <= x_celula <= x3_max and y3_min <= y_celula <= y3_max)
+                    (x3_min <= x_celula <= x3_max and y3_min <= y_celula <= y3_max) or
+                    (x4_min <= x_celula <= x4_max and y4_min <= y_celula <= y4_max)
                 ):
                     self.grid[r, c] = 1
 
